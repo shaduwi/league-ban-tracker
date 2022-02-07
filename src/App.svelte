52a2adad11139275569e9f3febf297f3c2fe348c
shaduwi <jp.vthoenderdaal@gmail.com>
2022-02-07 13:19:37 +0100

    Added routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,14 @@
 <script>
+  import { Router, Route, Link } from "svelte-routing";
+  import Home from "./routes/Home.svelte";
+  import Blog from "./routes/Result.svelte";
+
+  export let url = "";
 </script>
 
-<main class="bg-blue-gray-900 h-screen">
-  <div class="flex flex-col items-center w-screen">
-    <h1 class="text-white text-7xl mt-20">Is he banned yet?</h1>
-
-    <div class="relative mt-10">
-      <input
-        class="h-14 min-w-124 px-5 rounded-lg focus:shadow focus:outline-none"
-        type="text"
-        placeholder="Enter summoner name..."
-      />
-      <div class="absolute top-2 right-2">
-        <button
-          class="h-10 w-20 text-white rounded-lg bg-blue-500 hover:bg-blue-600"
-          >Search</button
-        >
-      </div>
-    </div>
+<Router {url}>
+  <div>
+    <Route path="/"><Home /></Route>
+    <Route path="result" component={Blog} />
   </div>
-</main>
+</Router>
