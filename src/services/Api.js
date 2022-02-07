48f8b9ec512fbb4fc68b4d2a9da54dccf09e08a9
Shaduwi <jp.vthoenderdaal@gmail.com>
2022-02-07 15:36:07 +0100

    removed unneeded post request

--- src/services/Api.js
+++ src/services/Api.js
@@ -27,11 +27,8 @@ const apiRequest = (method, url, request) => {
 
 // Make a GET request.
 const get = (url, request) => apiRequest("get", url, request);
-// Make a POST request.
-const post = (url, request) => apiRequest("post", url, request);
 
 const API = {
   get,
-  post,
 };
 export default API;
