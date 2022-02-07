54d1e7f5a2febe8a77fd34c0217c7a5af1f87b69
shaduwi <jp.vthoenderdaal@gmail.com>
2022-02-07 13:20:55 +0100

    added result page

---
+++ src/routes/Result.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { onMount } from "svelte";
+  import {
+    getSummonerByName,
+    getMatchesByPuuid,
+    getMatchById,
+  } from "../api/match";
+  const summonerName = "shaduwi";
+  const summonerPuuid = "";
+  getSummonerByName(summonerName).then((a) => {
+    console.log(a);
+  });
+  console.log(getSummonerByName(summonerName));
+</script>
+
+<main class="bg-blue-gray-900 h-screen">
+  <div class="flex flex-col items-center w-screen">
+    <h1 class="text-white text-7xl mt-20">yes he is banned :)</h1>
+    <h2 class="text-white text-3xl mt-5">the users name is: {summonerName}</h2>
+    <h2 class="text-white text-3xl mt-5">
+      the users puuid is: {summonerPuuid}
+    </h2>
+
+    <div class="relative mt-10">
+      <input
+        class="h-14 min-w-124 px-5 rounded-lg focus:shadow focus:outline-none"
+        type="text"
+        placeholder="Enter summoner name..."
+      />
+      <div class="absolute top-2 right-2">
+        <button
+          class="h-10 w-20 text-white rounded-lg bg-blue-500 hover:bg-blue-600"
+          >Search</button
+        >
+      </div>
+    </div>
+  </div>
+</main>
