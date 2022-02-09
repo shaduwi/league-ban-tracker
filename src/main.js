377fdc1fff948dfa089d04b5532ff6031b3a8461
shaduwi <jp.vthoenderdaal@gmail.com>
2022-02-09 19:44:02 +0100

    Added hydration

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import "virtual:windi-devtools";
 
 const app = new App({
   target: document.getElementById("app"),
+  hydrate: true,
 });
 
 export default app;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), WindiCSS()],
+  plugins: [
+    svelte({
+      compilerOptions: {
+        hydratable: true,
+      },
+    }),
+    WindiCSS(),
+  ],
 });
