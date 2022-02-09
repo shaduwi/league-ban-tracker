8de00e3ac172b38f874dac34b51331c2923ec190
shaduwi <jp.vthoenderdaal@gmail.com>
2022-02-09 15:24:13 +0100

    added summonerName component

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,22 +1,11 @@
 <script>
+  import SummonerInput from "../components/summonerInput.svelte";
+  export let summonerName;
 </script>
 
 <main class="bg-blue-gray-900 h-screen">
   <div class="flex flex-col items-center w-screen">
-    <h1 class="text-white text-7xl mt-20">Is he banned yet?</h1>
-
-    <div class="relative mt-10">
-      <input
-        class="h-14 min-w-124 px-5 rounded-lg focus:shadow focus:outline-none"
-        type="text"
-        placeholder="Enter summoner name..."
-      />
-      <div class="absolute top-2 right-2">
-        <button
-          class="h-10 w-20 text-white rounded-lg bg-blue-500 hover:bg-blue-600"
-          >Search</button
-        >
-      </div>
-    </div>
+    <h1 class="text-white text-7xl mt-20">Are they banned?</h1>
+    <SummonerInput name={summonerName} />
   </div>
 </main>
