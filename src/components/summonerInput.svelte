e2d909bedfbc94a46161b4d017b965a71c20d6d2
shaduwi <jp.vthoenderdaal@gmail.com>
2022-02-09 15:23:38 +0100

    added summonerInput component

---
+++ src/components/summonerInput.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let name;
+</script>
+
+<div class="relative mt-10">
+  <form action="/result/{name}">
+    <input
+      class="h-14 min-w-124 px-5 rounded-lg focus:shadow focus:outline-none"
+      type="text"
+      placeholder="Enter summoner name..."
+      bind:value={name}
+    />
+    <div class="absolute top-2 right-2">
+      <button
+        class="h-10 w-20 text-white rounded-lg bg-blue-500 hover:bg-blue-600"
+        >Search</button
+      >
+    </div>
+  </form>
+</div>
