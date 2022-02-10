4024ce323f068be2fc6bd91182d15bd634918b39
shaduwi <jp.vthoenderdaal@gmail.com>
2022-02-10 17:03:31 +0100

    Fixed result page

--- src/components/summonerInput.svelte
+++ src/components/summonerInput.svelte
@@ -7,11 +7,11 @@
       alert("You need to fill in a name");
       return;
     }
-    navigate("/result/" + name);
+    location.href = "/result/" + name;
   }
 </script>
 
-<div class="relative mt-10">
+<div class="relative">
   <form on:submit={onSubmit}>
     <input
       class="h-14 min-w-124 px-5 rounded-lg focus:shadow focus:outline-none"
@@ -21,7 +21,7 @@
     />
     <div class="absolute top-2 right-2">
       <button
-        class="h-10 w-20 text-white rounded-lg bg-blue-500 hover:bg-blue-600"
+        class="h-10 w-20 text-white rounded-lg bg-blue-700 hover:bg-blue-800"
         >Search</button
       >
     </div>
