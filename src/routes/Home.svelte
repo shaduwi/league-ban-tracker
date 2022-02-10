5dad355d446355699fdcb9d6fe2da3cf82912c23
shaduwi <jp.vthoenderdaal@gmail.com>
2022-02-10 17:03:49 +0100

    changed styling

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -3,9 +3,9 @@
   export let summonerName;
 </script>
 
-<main class="bg-blue-gray-900 h-screen">
+<main class="h-screen">
   <div class="flex flex-col items-center w-screen">
-    <h1 class="text-white text-7xl mt-20">Are they banned?</h1>
+    <h1 class="text-white text-7xl my-20">Are they banned?</h1>
     <SummonerInput name={summonerName} />
   </div>
 </main>
