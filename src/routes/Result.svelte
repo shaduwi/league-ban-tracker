3b3e30cc0ec23c7ab11912ea34395bf937bb632b
shaduwi <jp.vthoenderdaal@gmail.com>
2022-02-10 17:04:28 +0100

    Fixed bugs, added transition

--- src/routes/Result.svelte
+++ src/routes/Result.svelte
@@ -12,14 +12,30 @@
   let lastMatchMessage;
   let bannedMessage = "banned";
   let bannedDescription = "";
+  let finished = false;
 
   onMount(async () => {
-    summonerPuuid = await getSummonerByName(summonerName);
-    summonerPuuid = summonerPuuid.puuid;
+    let matchDate;
 
-    let match = await getMatchesByPuuid(summonerPuuid);
+    try {
+      summonerPuuid = await getSummonerByName(summonerName);
+      summonerPuuid = summonerPuuid.puuid;
+    } catch (error) {
+      finished = true;
+      console.log("hehehe");
+      bannedMessage = "an invalid account";
+      return;
+    }
 
-    let matchDate = await getMatchById(match);
+    try {
+      let match = await getMatchesByPuuid(summonerPuuid);
+      matchDate = await getMatchById(match);
+    } catch (error) {
+      finished = true;
+      lastMatchMessage = "not found";
+      return;
+    }
+
     matchDate = matchDate.info.gameEndTimestamp;
 
     let currTime = new Date().getTime();
@@ -35,41 +51,53 @@
     var minutes = Math.floor(dateDiff / 60) % 60;
     dateDiff -= minutes * 60;
 
-    let d = new Date(0);
-    d.setSeconds(dateDiff);
     if (matchDate !== undefined) {
       lastMatchMessage =
         days + " days " + hours + " hours " + minutes + " minutes ago";
     } else {
       lastMatchMessage = "not found";
     }
-    if (d.getDate() < 30) {
+    if (days < 30) {
       bannedMessage = "not banned";
     }
-    if (d.getDate() > 30) {
+    if (days > 30) {
       bannedMessage = "probably banned";
       bannedDescription = "has not played a game in the last 30 days";
     }
-    if (d.getDate() > 365) {
+    if (days > 365) {
       bannedMessage = "banned";
       bannedDescription = "has not played a game in a year";
     }
+    finished = true;
   });
 </script>
 
-<main class="bg-blue-gray-900 h-screen">
-  <div class="flex flex-col items-center w-screen">
+<main class="h-screen">
+  <div
+    class="flex flex-col items-center w-screen transition-hidden {finished
+      ? ''
+      : 'opacity-0'}"
+  >
     {#if summonerPuuid}
       <h1 class="text-white text-7xl mt-20">
         {summonerName} is {bannedMessage}
       </h1>
-      <h2 class="text-white text-3xl mt-5">
-        Their last match was {lastMatchMessage}
-      </h2>
-      <h4 class="text-gray-500">{bannedDescription}</h4>
+      {#if lastMatchMessage}
+        <h2 class="text-white text-3xl mt-5">
+          Their last match was {lastMatchMessage}
+        </h2>
+      {/if}
+
+      <h4 class="text-gray-500 my-5">{bannedDescription}</h4>
     {:else}
-      <h1 class="text-white text-7xl mt-20">This user was not found</h1>
+      <h1 class="text-white text-7xl my-20">This user was not found</h1>
     {/if}
     <SummonerInput name="" />
   </div>
 </main>
+
+<style>
+  :global(.transition-hidden) {
+    transition: opacity 0.5s ease-in;
+  }
+</style>
